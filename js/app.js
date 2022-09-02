39d16dbf7bddeade18e2cbeb92822bd2ada6d951
rakibul58 <rakibul58>
2022-09-02 21:44:19 +0600

    categories load done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('connected');
+const loadCategories = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data.news_category))
+    .catch(error => console.log(error));
+}
+
+const showCategories = (categories) => {
+    
+}
+
+loadCategories();
