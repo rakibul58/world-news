5ba43b1d56f76ec4eb7ec1fb9a3b59271636db5d
rakibul58 <rakibul58>
2022-09-03 09:59:34 +0600

    number of categories

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const loadCategories = () => {
+    toggle(true);
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => showCategories(data.data.news_category))
-    .catch(error => console.log(error));
+        .then(res => res.json())
+        .then(data => showCategories(data.data.news_category))
+        .catch(error => console.log(error));
 }
 
 const showCategories = (categories) => {
@@ -12,9 +13,9 @@ const showCategories = (categories) => {
 
         const newDiv = document.createElement('a');
         newDiv.innerHTML = `
-            <button id="${category.category_id}" onclick="loadCategoryNews('${category.category_id}')" class="btn fw-semibold fs-5 text-secondary my-btn" > ${ category.category_name } </button>`;
+            <button id="${category.category_id}" onclick="loadCategoryNews('${category.category_id}')" class="btn fw-semibold fs-5 text-secondary my-btn" > ${category.category_name} </button>`;
         categoriesDiv.appendChild(newDiv);
-        
+
     });
     const buttons = document.getElementsByClassName('my-btn');
     buttons[0].classList.remove('text-secondary');
@@ -22,18 +23,17 @@ const showCategories = (categories) => {
     loadCategoryNews('05');
 }
 
-const loadCategoryNews = (categoryId) =>{
-
+const loadCategoryNews = (categoryId) => {
+    toggle(true);
     selectedCategory(categoryId);
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => showCategoryNews(data.data))
-    .catch(error => console.log(error));
+        .then(res => res.json())
+        .then(data => showCategoryNews(data.data))
+        .catch(error => console.log(error));
 }
 
-const selectedCategory = (categoryId) =>
-{
+const selectedCategory = (categoryId) => {
     resetBtn();
     const selectBtn = document.getElementById(categoryId);
     selectBtn.classList.remove('text-secondary');
@@ -43,9 +43,9 @@ const selectedCategory = (categoryId) =>
 const resetBtn = () => {
     const buttons = document.getElementsByClassName('my-btn');
     for (const button of buttons) {
-        
-            button.classList.remove('text-primary');
-            button.classList.add('text-secondary');
+
+        button.classList.remove('text-primary');
+        button.classList.add('text-secondary');
     }
 }
 
@@ -53,13 +53,14 @@ const showCategoryNews = categoryItems => {
     const newsBlock = document.getElementById('news-block');
     newsBlock.textContent = '';
     console.log(categoryItems)
+    const numOfCategories = document.getElementById('number-of-categories');
+    numOfCategories.innerText = `${categoryItems.length ? categoryItems.length : 'No'} items found for category Entertainment`;
     categoryItems.forEach(categoryItem => {
         const newBlock = document.createElement('div');
-        newBlock.classList.add('row', 'g-0' , 'mb-5' , 'bg-white' , 'p-3' , 'rounded-4');
+        newBlock.classList.add('row', 'g-0', 'mb-5', 'bg-white', 'p-3', 'rounded-4');
         let details = categoryItem.details;
-        if(categoryItem.details.length > 500)
-        {
-            details = details.slice(0,500)+'...';
+        if (categoryItem.details.length > 500) {
+            details = details.slice(0, 500) + '...';
         }
         newBlock.innerHTML = `
 
@@ -78,7 +79,19 @@ const showCategoryNews = categoryItems => {
 
         `;
         newsBlock.appendChild(newBlock);
-    })
+    });
+    toggle(false);
+}
+
+const toggle = isLoading => {
+
+    if (isLoading) {
+        document.getElementById('loading').classList.remove('d-none');
+    }
+    else {
+        document.getElementById('loading').classList.add('d-none');
+    }
+
 }
 
 loadCategories();
